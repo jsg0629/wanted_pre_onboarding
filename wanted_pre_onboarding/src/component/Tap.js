5ce3d22bf3f678b074cebb9a0c540b8ad1e8de6d
jsg0629 <tnrms513@gmail.com>
2022-04-26 17:25:13 +0900

    :heavy_plus_sign: Requirement1 : Tap.js Done
    Active tap marker

--- wanted_pre_onboarding/src/component/Tap.js
+++ wanted_pre_onboarding/src/component/Tap.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Link  } from 'react-router-dom';
+import { useState } from "react";
+import './Tap.css';
+
 
 const Sidebar = () => {
+  const [ viewPoint, setViewPoint ] = useState(0); 
   return (
     <section className="tap">
-      <Link to={'/'}><i className="far fa-question-circle">Feed</i></Link>
-      <Link to={'/message'}><i className="far fa-comment-dots">Message</i></Link>
-      <Link to={'/mypage'}><i className="far fa-user">Mypage</i></Link>
+      <Link to={'/'} onClick={() => { setViewPoint(0); }} className={ viewPoint === 0 ? "tap__item tap__selleted__tit" : "tap__item tap__tit" }>Feed</Link>
+      <Link to={'/message'} onClick={() => { setViewPoint(1); }} className={ viewPoint === 1 ? "tap__item tap__selleted__tit" : "tap__item tap__tit" }>Message</Link>
+      <Link to={'/mypage'} onClick={() => { setViewPoint(2); }} className={ viewPoint === 2 ? "tap__item tap__selleted__tit" : "tap__item tap__tit" }>MyPage</Link>
+      <div class="active__tab__marker"></div>
     </section>
   );
 };
